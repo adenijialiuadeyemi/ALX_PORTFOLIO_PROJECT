a09a43a0d0a70d6d770245fc147680110a88feda
Adeniji ALiu Adeyemi <adenijialiuadeyemi@gmail.com>
2024-04-17 10:14:31 +0100

    Remove landing_page dir

--- Landing_Page/landing.js
+++
@@ -1,5 +0,0 @@
-// Smooth scroll to section when CTA button is clicked
-document.querySelector('.cta-btn').addEventListener('click', function() {
-    document.querySelector('#features').scrollIntoView({ behavior: 'smooth' });
-  });
-  

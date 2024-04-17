9cc503e8714e2266df48a46a98cc61a5993fe20d
Adeniji ALiu Adeyemi <adenijialiuadeyemi@gmail.com>
2024-04-17 05:18:49 +0100

    landing page created

---
+++ Landing_Page/landing.js
@@ -0,0 +1,5 @@
+// Smooth scroll to section when CTA button is clicked
+document.querySelector('.cta-btn').addEventListener('click', function() {
+    document.querySelector('#features').scrollIntoView({ behavior: 'smooth' });
+  });
+  
